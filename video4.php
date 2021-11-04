c8d781e371b2b978bb0d09ae913b8095dd49f7ae
PogramLearningWithPouyan <70820602+PogramLearningWithPouyan@users.noreply.github.com>
2021-11-04 16:36:03 +0330

    Add files via upload

---
+++ video4.php
@@ -0,0 +1,33 @@
+<?php
+// constants
+
+$name='pouyan';
+// $name='ali';
+// echo $name;
+
+// define('name','value');
+// define('name','ali');
+// echo defined('name');
+
+// const chanel_name='plp';
+// echo chanel_name;
+
+// if(true){
+//     define('name','ali');
+//     const chanel_name='plp';
+//     echo chanel_name;
+// }
+
+// define('first_'.$name,5);
+// echo first_pouyan;
+
+// echo PHP_VERSION;
+// echo __LINE__;
+
+// echo __FILE__;
+
+$x='a';
+$$x='b'; # $a='b;
+// echo $a;
+
+echo "$x , {$$x}";
