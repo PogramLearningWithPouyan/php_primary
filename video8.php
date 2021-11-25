91fd73e4b9729e1ef5f9b27558c98c32771c26f1
PogramLearningWithPouyan <70820602+PogramLearningWithPouyan@users.noreply.github.com>
2021-11-25 13:14:56 +0330

    Add files via upload

---
+++ video8.php
@@ -0,0 +1,33 @@
+<?php
+# float
+
+#$x=13.5e-3;
+#$x=13_000_222.5;
+#$x=PHP_FLOAT_MAX;
+#$x=ceil((0.1+0.7)*10);
+#7.99999999999999991118
+#$x=ceil((0.1+0.2)*10);
+#3.000000000004
+/*
+$x=0.23;
+$y=1-0.77; //0.23
+
+if ($x==$y){
+    echo 'yes';
+}else{
+    echo 'no';
+}
+*/
+#echo log(-1);
+#echo INF;
+#$x= PHP_FLOAT_MAX *2;
+
+#var_dump(is_infinite($x));
+#var_dump(is_finite($x));
+
+#$x=NAN;
+#var_dump(is_nan($x));
+
+$x='5.a2jhtdh';
+var_dump((float)$x);
+#var_dump(floatval($x));
