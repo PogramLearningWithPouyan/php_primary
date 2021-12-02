ce47abf292c4aa4aa405b1645adaa106f9cbff73
PogramLearningWithPouyan <70820602+PogramLearningWithPouyan@users.noreply.github.com>
2021-12-02 13:06:53 +0330

    Add files via upload

---
+++ video10.php
@@ -0,0 +1,23 @@
+<?php
+# null
+
+#null constant
+#$x=NULL;
+#echo $x;
+#var_dump(is_null($x));
+#var_dump($x===null);
+
+#not define
+
+#var_dump(is_null($t)) ;
+
+#remove
+/*
+$x=123;
+var_dump($x);
+unset($x);
+var_dump($x);
+*/
+
+$x=null;
+var_dump((array)$x);
