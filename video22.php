ebc741f694b4fe7e5f2f3e5bdf9bb1f80c084d3f
PogramLearningWithPouyan <70820602+PogramLearningWithPouyan@users.noreply.github.com>
2022-01-13 19:03:10 +0330

    Add files via upload

---
+++ video22.php
@@ -0,0 +1,38 @@
+<?php
+// Functions
+/*
+if (true){
+    function sayhello(){
+        echo 'hello';
+        echo '<br />';
+    }
+}
+sayhello();
+
+
+
+
+function x(){
+    $x=1;
+    $x++;
+    echo $x;
+}
+
+x();
+x();
+*/
+
+function sayhello(){
+    echo 'hello';
+    echo '<br />';
+    function saybye(){
+        echo 'bye';
+        echo '<br />';
+    }
+}
+sayhello();
+#saybye();
+
+function sayhello(){
+    echo 'hello2';
+}
